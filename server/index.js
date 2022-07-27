75c5124cbfff9be36edc0d9e463dede1f8139972
RomelClavel <romeletoty@gmail.com>
2022-07-27 10:31:34 +0200

    add: cors usage

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 const Express = require('express');
 const app = Express();
+const cors = require('cors');
 const router = require('./routes/router');
 const port = 3001;
 const host = 'localhost';
 
 app.use(Express.json());
+app.use(cors());
+
 app.use(router);
 
 app.listen(port, () => {
